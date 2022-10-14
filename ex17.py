8e5143fbd506eb07a1690c755d9acf8886fc342f
ArnavNmarktine <arnav.naithani@marktine.com>
2022-10-14 12:26:17 +0530

    ex17

--- ex17.py
+++ ex17.py
@@ -1,17 +1,18 @@
 from sys import argv 
 from os.path import exists 
+
 script,from_file,to_file =argv
 
 print(f"Copying from {from_file} to {to_file}")
 
 # we could do these two on one line, how ?
-
 in_file =open(from_file)
-
 indata =in_file.read()
 
 print(f"The input  file is {len(indata)} bytes long")
-print(f"Does, hot RETURN to continue, CTRL-C to abort.")
+
+print(f"Does the output file exist ? {exists(to_file)}")
+print(f"Ready ?, hit RETURN to continue, CTRL-C to abort.")
 input()
 
 
